5be786c757d082ebfeb3117e98791ce9b2cb1dad
JEREMIA-SUNNY <you@example.com>
2022-02-21 22:59:55 +0530

    new

---
+++ mcq.js
@@ -0,0 +1,56 @@
+const ready = document.querySelector(".readyBtn");
+const qstn = document.querySelector(".question");
+const par = document.querySelector("#parent")
+const readyBtn = document.querySelector(".ready");
+const sub = document.querySelector(".btn");
+const tim=document.querySelector(".timer")
+let count=0;
+
+function result() {
+    var score = 0;
+
+    if (document.getElementById('correct1').checked) {
+
+        score++;
+
+    }
+    if (document.getElementById('correct2').checked) {
+
+        score++;
+
+    }
+    if (document.getElementById('correct3').checked) {
+
+        score++;
+
+    }
+    if (document.getElementById('correct4').checked) {
+
+        score++;
+
+    } if (document.getElementById('correct5').checked) {
+
+        score++;
+
+    }
+    document.write("Your score is:" + score);
+}
+ready.addEventListener("click", () => {
+    console.log(par)
+    par.style.display = "grid";
+    readyBtn.remove(ready);
+    console.log(sub)
+    sub.style.display = "block";
+    console.log(tim)
+    const tm=setInterval(()=>{
+        count++; 
+        tim.innerHTML=count;
+        if(count===10){
+
+          result();
+            clearInterval(tm);
+        
+        }
+    },1000)
+
+});
